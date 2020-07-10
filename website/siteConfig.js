8fe8408934756de1b6a3570ac04c09f7bcd7c6e3
Zach Wild <zacharyjwild@gmail.com>
2020-07-10 16:12:52 -0400

    Reorganize docs directory

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,7 +39,6 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'intro', label: 'Docs'},
-    {doc: 'api', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
